982f15145943cd80745dcc5c747990edf1fb9639
Simon Blum <simon51100@gmail.com>
2013-04-09 14:47:33 +0200

    Reorganisation du code + modifications after reviews
    After reviews of Florent & Kaze and advices from Kaze about the
    "organization" of the code. I put libraries (OpenLayer et WebL10n ) in
    folder "libs". I edited runrunrun.js after review of Florent (aka ??)

--- js/runrunrun.js
+++ js/runrunrun.js
@@ -1,82 +1,66 @@
 'use strict';
 var id_visible = "home";
+window.addEventListener("load", function(){ 
 
-    window.onload = function() {
+  runrunrun.indexedDB.open();
+  switchButtonsStartToFix();
+  init_osm();
 
-      
-      
-      runrunrun.indexedDB.open();
-      switchButtonsStartToFix();
-      document.getElementById("logo").addEventListener("click", changeVisibility, false);
-      document.getElementById("top-titre").addEventListener("click", changeVisibility, false);
-      
+  
 
-      var elements_nav_item = document.getElementsByClassName("nav-item");
-      var elements_btn_fly = document.getElementsByClassName("button_fly");
-      var elements_btn_action = document.getElementsByClassName("button_action");
-      
-      for(var i in elements_nav_item) {
-        if(elements_nav_item[i] instanceof Element){
-          elements_nav_item[i].addEventListener("click", changeVisibility, false); 
-        }
-      };
+  var elements = document.querySelectorAll("#logo, #top-titre, .nav-item, .button_fly");
 
-      for(var j in elements_btn_fly) {
-        if(elements_btn_fly[j] instanceof Element){
-          elements_btn_fly[j].addEventListener("click", changeVisibility, false); 
-        }
-      };
+  for (var i = 0; i < elements.length; i++){
+        elements[i].addEventListener("click", changeVisibility);
+  } 
 
-      for(var m=0; m<elements_btn_action.length;  m++){
-        elements_btn_action[m].addEventListener("click", function (){ executeAction(this);}, false); 
-      }
-     
+  
+  var elements_btn_action = document.getElementsByClassName("button_action");
 
-      init_osm();
-      var flyto=function (id){
-        console.log(id);
-        document.getElementById(id).style.display = 'inline';
-        document.getElementById(id_visible).style.display = 'none';
-        id_visible = id;
-      }
-       
-      var executeAction=function(obj){
-        var action=obj.getAttribute("data-action");
-        eval(action); 
-        
-      }
-     
+  for(var m=0; m<elements_btn_action.length;  m++){
+    elements_btn_action[m].addEventListener("click", function (){ executeAction(this);}, false); 
+  }
 
-      function changeVisibility() {
-        
-        var id = this.getAttribute("data-role");
+  var flyto=function (id){
+    document.getElementById(id).style.display = 'inline';
+    document.getElementById(id_visible).style.display = 'none';
+    id_visible = id;
+  }
 
-        if(id == 'home') {
-          var elements = document.getElementsByClassName('page');
+  var executeAction=function(obj){
+    var action=obj.getAttribute("data-action");
+    eval(action); 
+  }
 
-          for(var i = 0, length = elements.length; i < length; i++) {
-            elements[i].style.display = 'none';
-          }
+  function changeVisibility() {
+    var id = this.getAttribute("data-role");
 
-          document.getElementById("home").style.display = 'inline';
-          id_visible = "home";
-        } else {
+    if(id == 'home') {
+      var elements = document.getElementsByClassName('page');
 
-          if(!isVisible(id)){
-            document.getElementById(id).style.display = 'inline';
-            document.getElementById(id_visible).style.display = 'none';
-            id_visible = id;
-          }
-        }
-      }
-    
-      
-      function isVisible(id) {
-        
-        if(document.getElementById(id).style.display == "inline")
-           return true;
-        else
-           return false;
+      for(var i = 0, length = elements.length; i < length; i++) {
+        elements[i].style.display = 'none';
       }
 
+      document.getElementById("home").style.display = 'inline';
+      id_visible = "home";
+    } else {
+      var elem=document.getElementById(id);
+      if(!isVisible(elem)){
+        flyto(id);
+      }
     }
+  }
+
+  function isVisible(elem){
+    return elem.style.display == "inline";
+  } 
+
+}); 
+
+
+      
+      
+     
+
+ 
